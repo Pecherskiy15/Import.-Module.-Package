63c06fee417cb845e699c4d0715bfc4d35404056
Aleksander Pecherskiy <111129041+Pecherskiy15@users.noreply.github.com>
2023-06-04 15:32:36 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from salary import calculate_salary
+from people import get_employees
+from datetime import datetime
+
+if __name__ == '__main__':
+    print(datetime.now())
+    print(calculate_salary())
+    print(get_employees())
