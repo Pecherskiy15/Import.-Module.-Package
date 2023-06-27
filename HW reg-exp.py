c53288005d6797e005dbf56f7b45b0bf44ebbeb2
Aleksander Pecherskiy <111129041+Pecherskiy15@users.noreply.github.com>
2023-06-27 14:25:11 +0300

    Update HW reg-exp.py

--- HW reg-exp.py
+++ HW reg-exp.py
@@ -1,68 +1,66 @@
-from pprint import pprint
-## Читаем адресную книгу в формате CSV в список contacts_list:
-import csv
 import re
+import csv
 
 with open("/Users/aleksanderpecherskiy/Desktop/my_demo/py-homeworks-advanced/5.Regexp/phonebook_raw.csv") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
+    updated_contact_list = []
 
-# pprint(contacts_list)
+def change_names():
+    name_pattern = r'([А-Я])'
+    count_sub = r' \1'
+    for column in contacts_list[1:]:
+        fio = column[0] + column[1] + column[2]
+        if len(re.sub(name_pattern, count_sub, fio).split()) == 3:
+            column[0] = re.sub(name_pattern, count_sub, fio).split()[0]
+            column[1] = re.sub(name_pattern, count_sub, fio).split()[1]
+            column[2] = re.sub(name_pattern, count_sub, fio).split()[2]
+        elif len(re.sub(name_pattern, count_sub, fio).split()) == 2:
+            column[0] = re.sub(name_pattern, count_sub, fio).split()[0]
+            column[1] = re.sub(name_pattern, count_sub, fio).split()[1]
+            column[2] = ''
+        elif len(re.sub(name_pattern, count_sub, fio).split()) == 1:
+            column[0] = re.sub(name_pattern, count_sub, fio).split()[0]
+            column[1] = ''
+            column[2] = ''
+    return
 
-## 1. Выполните пункты 1-3 задания.
+def change_phone_number():
+    phone_pattern = re.compile(r'(\+7|8)?\s*\(?(\d{3})\)?\s*\D?(\d{3})[-\s+]?(\d{2})-?(\d{2})((\s)?\(?(доб.)?\s?(\d+)\)?)?')
+    phone_sub = r'+7(\2)\3-\4-\5\7\8\9'
+    for column in contacts_list:
+        column[5] = phone_pattern.sub(phone_sub, column[5])
+    return
 
-result_list, phone, name, organisation, position, email, result_dict = [], [], [], [], [], [], {}
-buff = ""
-for i in range(1, len(contacts_list)):
-    res = (re.findall(r'\d+\s*', ''.join(contacts_list[i])))
-    phone.append((''.join(res)).replace(' ', ""))
+def duplicate_fio():
+    distinct_list = {}
+    for column in contacts_list[1:]:
+        last_name = column[0]
+        if last_name not in distinct_list:
+            distinct_list[last_name] = column
+        else:
+            for indic, item in enumerate(distinct_list[last_name]):
+                if item == '':
+                    distinct_list[last_name][indic] = column[indic]
 
-    buff = ""
-    for j in range(3):
-         if len(contacts_list[i][j]) > 1:
-            buff += " " + contacts_list[i][j]
-    name.append(buff)
-    organisation.append(contacts_list[i][3])
-    position.append(contacts_list[i][4])
-    email.append(contacts_list[i][6])
+    for last_name, contact in distinct_list.items():
+        for contacts in contact:
+            if contact not in updated_contact_list:
+                updated_contact_list.append(contacts)
 
-for i in range(len(phone)):
-    if phone[i]:
-        phone[i] = f'+7({phone[i][1:4]}){phone[i][3:]}'
-    if len(phone[i]) > 15:
-        phone[i] = f'{phone[i][0:15]} доб. {phone[i][15:]}'
+    return updated_contact_list
 
-result_list = zip(name,organisation,position,phone,email)
-result_list = list(result_list)
 
-for i in range(len(result_list)):
-    result_dict[" ".join(result_list[i][0].split()[0:2])]= {'last_name':" ".join(result_list[i][0].split()[0:1])}
-    result_dict[" ".join(result_list[i][0].split()[0:2])].update({'first_name':" ".join(result_list[i][0].split()[1:2])})
-    if len(result_list[i][0].split()[2:3]) > 0:
-        result_dict[" ".join(result_list[i][0].split()[0:2])].update({'surname':" ".join(result_list[i][0].split()[2:3])})
-    else:
-        result_dict[" ".join(result_list[i][0].split()[0:2])].update({'surname': "Информации нет"})
-    if len(result_list[i][1]) > 0:
-        result_dict[" ".join(result_list[i][0].split()[0:2])].update({'organization':result_list[i][1]})
-    else:
-        result_dict[" ".join(result_list[i][0].split()[0:2])].update({'organization': "Информации нет"})
-    if len(result_list[i][2]) > 0:
-        result_dict[" ".join(result_list[i][0].split()[0:2])].update({'position':result_list[i][2]})
-    else:
-        result_dict[" ".join(result_list[i][0].split()[0:2])].update({'organization': "Информации нет"})
-    if len(result_list[i][3]) > 0:
-        result_dict[" ".join(result_list[i][0].split()[0:2])].update({'phone':result_list[i][3]})
-    else:
-        result_dict[" ".join(result_list[i][0].split()[0:2])].update({'phone': "Информации нет"})
-    if len(result_list[i][4]) > 0:
-        result_dict[" ".join(result_list[i][0].split()[0:2])].update({'email':result_list[i][4]})
-    else:
-        result_dict[" ".join(result_list[i][0].split()[0:2])].update({'email': "Информации нет"})
 
-## 2. Сохраните получившиеся данные в другой файл.
-## Код для записи файла в формате CSV:
-with open("/Users/aleksanderpecherskiy/Desktop/my_demo/py-homeworks-advanced/5.Regexp/phonebook.csv", "w") as f:
-    datawriter = csv.writer(f, delimiter=',')
+if __name__ == '__main__':
+    with open("/Users/aleksanderpecherskiy/Desktop/my_demo/py-homeworks-advanced/5.Regexp/phonebook_raw.csv") as f:
+        rows = csv.reader(f, delimiter=",")
+        contacts_list = list(rows)
+        updated_contact_list = []
+        change_names()
+        change_phone_number()
+        duplicate_fio()
 
-    ## Вместо contacts_list подставьте свой список:
-    datawriter.writerows(result_dict)
+    with open("/Users/aleksanderpecherskiy/Desktop/my_demo/py-homeworks-advanced/5.Regexp/phonebook.csv", "w") as f:
+        datawriter = csv.writer(f, delimiter=',')
+        datawriter.writerows(contacts_list)
